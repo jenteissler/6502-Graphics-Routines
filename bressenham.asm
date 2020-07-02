038bdd9018b78d92ae07d4868529045def659c9f
Graphica <jteissler@uga.edu>
2020-07-02 16:41:48 -0400

    all diagonals working

--- bressenham.asm
+++ bressenham.asm
@@ -1,10 +1,10 @@
 ; Bressenham Line drawing
 
-LDA #30; x0 
+LDA #03; x0 
 STA $00 
 LDA #04 ; y0
 STA $01
-LDA #03 ; x1
+LDA #30 ; x1
 STA $02
 LDA #20 ; y1
 STA $03
@@ -13,83 +13,67 @@ JSR draw_line
 JMP exit
 
 draw_line: ; $00 = x0, $01 = y0, $02 = x1, $03 = y1
+  LDA #0
+  STA $06 ; initialize x flag
+  STA $07 ; initialize y flag
+  STA $08 ; initialize slope flag
+
+  LDA $00
+  CMP $02
+  BCC dl_x_complement ; take twos-complement of x1 & x0 if x1 < x0
+  LDA $00
+  JSR twos_complement
+  CLC
+  ADC #31
+  STA $00
+  LDA $02
+  JSR twos_complement
+  CLC
+  ADC #31
+  STA $02
   LDA #1
-  STA $06 ; initialize x counter
-  STA $07 ; initialize y counter
-  STA $08 ; initalize slope flag
+  STA $06 ; set x flag
+  dl_x_complement:
 
-; swap start and end coords if any delta is negative
-;  LDA $00
-;  CMP $02
-;  BCC dl_no_x_swap
- ; LDA $00
- ; PHA
-;  LDA $02
-;  STA $00
-;  PLA
-;  STA $02
-;  dl_no_x_swap:
-
-;  LDA $01
- ; CMP $03
-;  BCC dl_no_y_swap
-;  LDA $01
-;  PHA
-;  LDA $03
- ; STA $01
-  ;PLA
-  ;STA $03
-  ;dl_no_y_swap:
-  ; END CHANGE
+  LDA $01
+  CMP $03
+  BCC dl_y_complement ; take twos-complement of y1 & y0 if y1 < y0
+  LDA $01
+  JSR twos_complement
+  CLC
+  ADC #31
+  STA $01
+  LDA $03
+  JSR twos_complement
+  CLC
+  ADC #31
+  STA $03
+  LDA #1 
+  STA $07 ; set y flag
+  dl_y_complement:
 
   CLC
   LDA $02
   SBC $00  ; dx = x1 - x0
-  BPL dl_dx_positive 
-  JSR twos_complement ; correct negative delta
-  LDX #$FF
-  STA $06
-  ;PHA
-  ;LDA #$FF ; set x counter to -1
-  ;STA $06
-  ;PLA
-  dl_dx_positive:
   STA $04
 
   CLC
   LDA $03
-  TAY ; save y1
+  TAY ; save y1 to set as loop end
   SBC $01 ; dy = y1 - y0
-  BPL dl_dy_positive
-  JSR twos_complement ; correct negative delta
-  STX $07
-  ;PHA
-  ;LDA #$FF ; set y counter to -1
-  ;STA $07
-  ;PLA
-  dl_dy_positive:
   STA $03
 
-  ; if dy greater swap?
+  ; swap x and y to handle slope > 0.5
   CMP $04
-  BCC dl_swap_skip
+  BCC dl_slope
+  BEQ dl_slope
   STY $02 ; set y as end loop
-
   LDX $04 ; swap dy and dx
   STA $04
   STX $03
-
-  LDY $07 ; swap x and y counters
-  LDX $06
-  STY $06
-  STX $07
-
-  LDX #0 ; set slope flag
+  LDX #1 ; set slope flag
   STX $08
-
-
-  dl_swap_skip:
-
+  dl_slope:
 
   CLC
   ASL A
@@ -101,34 +85,21 @@ draw_line: ; $00 = x0, $01 = y0, $02 = x1, $03 = y1
   SBC $04
   ASL A
   STA $04 ; k = 2 (dy - dx) 
-
   ASL $03 ; i = 2dy
 
-  LDX $00
+  LDX $00 ; transfer loop variables to registers
   LDY $01
   LDA $05
 
-; loop should be whichever delta is greater
   dl_loop:
-    PHA ; push registers to stack
+    PHA
     TXA
     PHA
     TYA
     PHA
-
-    LDA #1
-    CMP $08
-    BEQ dll_invert_skip
-    TXA
-    PHA
-    TYA
-    TAX
-    PLA
-    TAY
-    dll_invert_skip:
-
+    JSR handle_flags
     JSR draw_pixel
-    PLA ; pull registers from stack
+    PLA
     TAY 
     PLA
     TAX 
@@ -139,7 +110,7 @@ draw_line: ; $00 = x0, $01 = y0, $02 = x1, $03 = y1
     PHA ; error (k) E > 0
     TYA
     CLC
-    ADC $07 ; y = y +- 1
+    ADC #1 ; y = y + 1
     TAY
     PLA
     ;INY
@@ -153,10 +124,9 @@ draw_line: ; $00 = x0, $01 = y0, $02 = x1, $03 = y1
     PHA
     TXA
     CLC
-    ADC $06 ; x = x +- 1
+    ADC #1 ; x = x + 1
     TAX
     PLA
-    ;INX
 
     CPX $02
     BEQ dl_loop_exit
@@ -187,14 +157,61 @@ draw_pixel: ; X = [0,31], Y = [0,31]
   STA ($10),Y 
   RTS
 
+handle_flags:
+  LDA $08
+  CMP #0
+  BEQ hf_end_slope
+  TXA
+  PHA
+  TYA
+  TAX
+  PLA
+  TAY
+  hf_end_slope:
+
+  LDA $06
+  CMP #0
+  BEQ hf_end_x
+  TXA
+  JSR twos_complement
+  CLC
+  ADC #31
+  TAX
+  hf_end_x:
+
+  LDA $07
+  CMP #0
+  BEQ hf_end_y
+  TYA 
+  JSR twos_complement
+  CLC
+  ADC #31
+  TAY
+  hf_end_y:
+  RTS
+
 twos_complement:
   EOR #$FF
   SEC
   ADC #0
   RTS
 
-multiply_unsigned:
-  
+push_registers:
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+  RTS
+
+pull_registers:
+  PLA
+  TAY 
+  PLA
+  TAX 
+  PLA
+  RTS
+
 
 
 exit:
